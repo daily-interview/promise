396892f50e7edfcd573e25c4babb2f6c8de1fa40
artdong <1029278668@qq.com>
2019-12-05 17:36:58 +0800

    feat:添加deferred方法,支持promises-aplus-tests

--- index.js
+++ index.js
@@ -49,4 +49,13 @@ MyPromise.prototype.then = function (onFulfilled, onRejected) {
     }
 };
 
+MyPromise.deferred = function() {
+    let dfd = {};
+    dfd.promise = new MyPromise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+}
+
 module.exports = MyPromise;

616a00758e193b78112e1fcbdae7ff7be1967503
artdong <1029278668@qq.com>
2019-12-05 17:22:47 +0800

    feat:实现resolve&reject

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
 function MyPromise(executor) {
+    let _this = this; // 拷贝this指针
     this.state = PENDING; // 初始状态
     this.value = undefined; // 成功结果
     this.reason = undefined; // 失败原因
@@ -11,11 +12,19 @@ function MyPromise(executor) {
     executor(resolve, reject); // 立即执行
 
     function resolve(value) {
-        // todo
+         // 当状态为pending时再做更新
+         if (_this.state === PENDING) {
+            _this.value = value; // 保存成功结果
+            _this.state = FULFILLED;
+        }
     }
 
     function reject(reason) {
-        // todo
+        // 当状态为pending时再做更新
+        if (_this.state === PENDING) {
+            _this.reason = reason; // 保存失败原因
+            _this.state = REJECTED;
+        }
     }
 }
 

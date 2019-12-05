99c84e4f5b7798d5505f7b79cc4b9811d3486db4
dongxingbin <dongxingbin@zbj.com>
2019-12-05 17:01:50 +0800

    feat:代码基本结构

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// Promise 的三种状态
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
 function MyPromise(executor) {
-    // todo
+    this.state = PENDING; // 初始状态
+    this.value = undefined; // 成功结果
+    this.reason = undefined; // 失败原因
+
+    function resolve(value) {
+        // todo
+    }
+
+    function reject(reason) {
+        // todo
+    }
 }
+
+Promise.MyPromise.then = function (onFulfilled, onRejected) {
+    // todo
+};
 
 module.exports = MyPromise;
